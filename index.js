7c69babb2a6283579f87dcca7c9ca639d1c3e858
Shashwat-Choudhari <sc.21u10640@btech.nitdgp.ac.in>
2024-10-27 18:44:41 +0530

    fix cors config

--- index.js
+++ index.js
@@ -10,11 +10,7 @@ const app = express();
 const port = 3000;
 
 app.use(bodyParser.json());
-app.use(cors({
-    origin: 'https://key-nest-frontend.vercel.app',
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    credentials: true,
-}));
+app.use(cors());
 
 const uri = process.env.MONGO_URI;
 
